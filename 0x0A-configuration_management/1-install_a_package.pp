d7e28d707056c2082a6c5b8691a711a4d818dddc
Amirou DIALLO <amiroudiallo.yw@gmail.com>
2024-03-22 17:39:46 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,6 @@
 # Ensure python3-pip is installed
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Define the command to install Flask 2.1.0
-$flask_install_command = '/usr/bin/pip3 install Flask==2.1.0'
-
-# Install Flask only if it's not already installed
-exec { 'install_flask':
-  command     => $flask_install_command,
-  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  path        => ['/usr/local/bin', '/usr/bin', '/bin'],
-  require     => Package['python3-pip'],
+package { 'flask':
+  ensure => '2.1.0',
+  provider => 'python3-pip'
 }
 
