1e77cc9c25c0ea3734f6151f7b666bfa50d4bebf
amiroudiallo-dev <amiroudiallo.yw@gmail.com>
2024-05-07 13:28:46 +0000

    0-

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     subscribers (not active users, total subscribers) for a given subreddit
     """
     subscribers = 0
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'advanced-api/0.0.1 by Mendy'}
     req = requests.get(url=url, headers=headers, allow_redirects=False)
     if req.status_code == 200:
